2e71524edc2e010d2ac14f5d6f173796b7b0d32e
mzahran1999 <76142922+mzahran1999@users.noreply.github.com>
2024-06-15 00:17:30 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const startSnakeGameButton = document.getElementById('startSnakeGame');
-    const messageSection = document.getElementById('message');
-
-    // Start Snake game on button click
-    startSnakeGameButton.addEventListener('click', function() {
-        const snakeGameSection = document.getElementById('snake-game');
-        snakeGameSection
-//ssss
