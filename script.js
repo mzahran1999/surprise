82e87f3ef4d9c8a954ee5c345ef305b960aac577
mzahran1999 <76142922+mzahran1999@users.noreply.github.com>
2024-06-14 23:35:34 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const startSnakeGameButton = document.getElementById('startSnakeGame');
+    const messageSection = document.getElementById('message');
+
+    // Start Snake game on button click
+    startSnakeGameButton.addEventListener('click', function() {
+        const snakeGameSection = document.getElementById('snake-game');
+        snakeGameSection
